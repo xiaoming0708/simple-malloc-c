d7e6f19d21d7b0809a8197d1623aa8e928069c30
Longzheng Han <3085934171@qq.com>
2025-12-30 20:51:07 +0800

    Refactor comments and improve code clarity in my_malloc.h

--- my_malloc.h
+++ my_malloc.h
@@ -1,11 +1,10 @@
 // my_malloc.h
-#ifndef MY_MALLOC_H  // 防止重复引用的“卫哨”
+#ifndef MY_MALLOC_H  // 防止重复引用
 #define MY_MALLOC_H
 
-#include <stddef.h>  // 为了 size_t
+#include <stddef.h>  //  size_t
 
-// 1. 把结构体定义搬到这里
-// 这样 main.c 和 my_malloc.c 都能认识什么是 block
+// 1. 结构体定义
 typedef struct block
 {
     size_t size;
@@ -13,9 +12,9 @@ typedef struct block
     struct block *next;
 } block;
 
-// 2. 函数声明 (不要写函数体，只写分号)
+// 2. 函数声明
 void *my_alloc(size_t size);
 void my_free(void *ptr);
-void debug_heap();  // 把调试工具也暴露出来方便测试
+void debug_heap();  
 
 #endif
